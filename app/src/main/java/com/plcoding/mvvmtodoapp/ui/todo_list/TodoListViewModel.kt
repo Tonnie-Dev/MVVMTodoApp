e13beb2bd6ff55de6d3f1a60532ca3fbe92c7916
Antony <vontonnie@gmail.com>
2022-01-01 15:08:35 +0300

    Add sendUIEvent viewmodel fxn

--- app/src/main/java/com/plcoding/mvvmtodoapp/ui/todo_list/TodoListViewModel.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/ui/todo_list/TodoListViewModel.kt
@@ -1,12 +1,15 @@
 package com.plcoding.mvvmtodoapp.ui.todo_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.plcoding.mvvmtodoapp.data.TodoRepository
 import com.plcoding.mvvmtodoapp.ui.todo_list.TodoListEvent.*
+import com.plcoding.mvvmtodoapp.util.Routes
 import com.plcoding.mvvmtodoapp.util.UIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,6 +30,16 @@ class TodoListViewModel @Inject constructor(private val repository: TodoReposito
             is OnUndoneDeleteClick ->{}
             is OnTodoClick ->{}
             is OnAddTodoClick ->{}
+        }
+    }
+
+
+
+    private fun sendUIEvent(event: UIEvent){
+
+        viewModelScope.launch {
+            //send() needs to be done in Coroutine context
+            _uiEvent.send(UIEvent.Navigate(Routes.ADD_EDIT_LIST))
         }
     }
 }
