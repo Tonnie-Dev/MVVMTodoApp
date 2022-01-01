40c8c737d71ea66808fab9334f970512570fdbbd
Antony <vontonnie@gmail.com>
2022-01-01 11:06:40 +0300

    Create TodoDatabase

--- app/src/main/java/com/plcoding/mvvmtodoapp/data/TodoDAO.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/data/TodoDAO.kt
@@ -1,24 +1,24 @@
 package com.plcoding.mvvmtodoapp.data
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoDAO {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert()
+    suspend fun insertTodo(todo: Todo)
 
+    //get realtime update in case database changes
     @Query("SELECT * FROM Todo")
-    suspend fun getTodos(): List<Todo>
+    suspend fun getTodos(): Flow<List<Todo>>
 
-    @Query("DELETE FROM Todo WHERE id=:id")
-    suspend fun deleteTodo(id: Int)
+    @Delete
+    suspend fun deleteTodo(todo: Todo)
 
+    //return nullable Todos if the id doesn't exist instead of crashing the app
     @Query("SELECT * FROM Todo WHERE id = :id")
-    suspend fun getTodoById(id: Int):Todo
+    suspend fun getTodoById(id: Int):Todo?
 
 
 }
