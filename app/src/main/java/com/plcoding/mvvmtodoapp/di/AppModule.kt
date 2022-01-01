1511c7b6f0603e03caf7284c7e10d11a8d2425f9
Antony <vontonnie@gmail.com>
2022-01-01 11:30:20 +0300

    Add provideDatabase fxn

--- app/src/main/java/com/plcoding/mvvmtodoapp/di/AppModule.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/di/AppModule.kt
@@ -1,14 +1,32 @@
 package com.plcoding.mvvmtodoapp.di
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import com.plcoding.mvvmtodoapp.data.TodoDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule{
 
     //DATABASE
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context):TodoDatabase{
+
+
+        return Room.databaseBuilder(context,TodoDatabase::class.java,"TodoDatabase").build()
+
+
+    }
     //REPOSITORY
+
+    
 
 }
