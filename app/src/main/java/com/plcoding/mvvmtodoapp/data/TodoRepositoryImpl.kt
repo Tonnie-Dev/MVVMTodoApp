e442aa3abb4dedb6cdc069956b464c1f324977d1
Antony <vontonnie@gmail.com>
2022-01-01 11:20:54 +0300

    Create TodoRepositoryImpl

---
+++ app/src/main/java/com/plcoding/mvvmtodoapp/data/TodoRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.plcoding.mvvmtodoapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+class TodoRepositoryImpl(private val dao: TodoDAO): TodoRepository {
+
+    override suspend fun insertTodo(todo: Todo) {
+
+        dao.insertTodo(todo = todo)
+    }
+
+    override suspend fun getTodos(): Flow<List<Todo>> {
+        return dao.getTodos()
+    }
+
+    override suspend fun deleteTodo(todo: Todo) {
+       dao.deleteTodo(todo = todo )
+    }
+
+    override suspend fun getTodoById(id: Int): Todo? {
+
+        dao.getTodoById(id = id)
+    }
+}
