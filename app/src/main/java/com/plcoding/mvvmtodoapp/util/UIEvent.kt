84792b36518f14ddb262b4352bcc870d21db7eff
Antony <vontonnie@gmail.com>
2022-01-01 14:34:06 +0300

    Add UIEvent sealed class

---
+++ app/src/main/java/com/plcoding/mvvmtodoapp/util/UIEvent.kt
@@ -0,0 +1,11 @@
+package com.plcoding.mvvmtodoapp.util
+
+sealed class UIEvent{
+
+
+    //navigate back
+    object PopBackStack:UIEvent()
+    data class Navigate(val route:String):UIEvent()
+    data class ShowSnackbar(val message:String,
+    val action:String? = null)
+}
