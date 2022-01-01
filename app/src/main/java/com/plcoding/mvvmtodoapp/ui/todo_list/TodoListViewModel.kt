3381ae23aef68ca10e9122e69837a9f429e4d110
Antony <vontonnie@gmail.com>
2022-01-01 14:59:43 +0300

    Add onEvent() fxn in ViewModel

--- app/src/main/java/com/plcoding/mvvmtodoapp/ui/todo_list/TodoListViewModel.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/ui/todo_list/TodoListViewModel.kt
@@ -2,10 +2,31 @@ package com.plcoding.mvvmtodoapp.ui.todo_list
 
 import androidx.lifecycle.ViewModel
 import com.plcoding.mvvmtodoapp.data.TodoRepository
+import com.plcoding.mvvmtodoapp.ui.todo_list.TodoListEvent.*
+import com.plcoding.mvvmtodoapp.util.UIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class TodoListViewModel @Inject constructor(private val repository:TodoRepository) :
-        ViewModel() {
+class TodoListViewModel @Inject constructor(private val repository: TodoRepository) :
+    ViewModel() {
+
+
+    // val todos = repository.getTodos()
+    private val _uiEvent = Channel<UIEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onEvent(event: TodoListEvent){
+
+        when(event){
+
+            is DeleteTodo ->{}
+            is OnDoneChange ->{}
+            is OnUndoneDeleteClick ->{}
+            is OnTodoClick ->{}
+            is OnAddTodoClick ->{}
+        }
+    }
 }
---
+++ app/src/main/java/com/plcoding/mvvmtodoapp/util/Routes.kt
@@ -0,0 +1,4 @@
+package com.plcoding.mvvmtodoapp.util
+
+object Routes {
+}
