d4a73d1fc8bd842c7bf1cb99e9e2c927f8e67003
Antony <vontonnie@gmail.com>
2022-01-01 11:15:27 +0300

    Add Repository interface

---
+++ app/src/main/java/com/plcoding/mvvmtodoapp/data/TodoRepository.kt
@@ -0,0 +1,22 @@
+package com.plcoding.mvvmtodoapp.data
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+interface TodoRepository {
+
+
+      suspend fun insertTodo(todo: Todo)
+
+
+    suspend fun getTodos(): Flow<List<Todo>>
+
+
+    suspend fun deleteTodo(todo: Todo)
+
+
+    suspend fun getTodoById(id: Int):Todo?
+}
