972b68b9c7c5eb6f78c2d24cc911e888f004954f
Antony <vontonnie@gmail.com>
2022-01-01 14:41:44 +0300

    Add TodoListEvent subclasses

--- app/src/main/java/com/plcoding/mvvmtodoapp/ui/todo_list/TodoListEvent.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/ui/todo_list/TodoListEvent.kt
@@ -1,6 +1,12 @@
 package com.plcoding.mvvmtodoapp.ui.todo_list
 
-sealed class TodoListEvent{
+import com.plcoding.mvvmtodoapp.data.Todo
 
-    
+sealed class TodoListEvent {
+
+    data class DeleteTodo(val todo: Todo) : TodoListEvent()
+    data class OnDoneChange(val todo: Todo, val isDone: Boolean) : TodoListEvent()
+    object OnUndoneDeleteClick : TodoListEvent()
+    data class OnTodoClick(val todo: Todo) : TodoListEvent()
+    object OnAddTodoClick : TodoListEvent()
 }
