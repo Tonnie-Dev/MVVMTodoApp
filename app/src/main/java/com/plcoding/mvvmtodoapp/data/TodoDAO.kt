fbdd92f8f7ba13f324df1a516a863ca2148af41d
Antony <vontonnie@gmail.com>
2022-01-01 10:35:24 +0300

    Add getTodos() dao fxn

--- app/src/main/java/com/plcoding/mvvmtodoapp/data/TodoDAO.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/data/TodoDAO.kt
@@ -3,10 +3,15 @@ package com.plcoding.mvvmtodoapp.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface TodoDAO {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert()
+
+    @Query("SELECT * FROM Todo")
+    suspend fun getTodos():List<Todo>
+    
 }
