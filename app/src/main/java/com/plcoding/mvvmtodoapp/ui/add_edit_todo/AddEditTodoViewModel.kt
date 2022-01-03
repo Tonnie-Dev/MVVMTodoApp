e445aa032542ccf5d79ff9567fe72681fb8ea6d3
Antony <vontonnie@gmail.com>
2022-01-03 08:44:01 +0300

    AddEditTodoViewModel

---
+++ app/src/main/java/com/plcoding/mvvmtodoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
@@ -0,0 +1,10 @@
+package com.plcoding.mvvmtodoapp.ui.add_edit_todo
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AddEditTodoViewModel @Inject constructor(private val) :
+    ViewModel() {
+}
