3c1c97b27c7bea308fdbea31dda1905e198f6188
Antony <vontonnie@gmail.com>
2022-01-03 08:51:57 +0300

    Add mutable states to the ViewModel

--- app/src/main/java/com/plcoding/mvvmtodoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
@@ -1,10 +1,22 @@
 package com.plcoding.mvvmtodoapp.ui.add_edit_todo
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.plcoding.mvvmtodoapp.data.Todo
+import com.plcoding.mvvmtodoapp.data.TodoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class AddEditTodoViewModel @Inject constructor(private val) :
+class AddEditTodoViewModel @Inject constructor(private val repository: TodoRepository) :
     ViewModel() {
+
+    var todo: Todo? = mutableStateOf(null)
+        private set
+
+    var title = mutableStateOf("")
+        private set
+
+    var description = mutableStateOf("")
+        private set
 }
