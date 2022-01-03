f5ac9edfccc51a21efb1800db0959212f4c0942a
Antony <vontonnie@gmail.com>
2022-01-03 07:54:12 +0300

    Retrive todo from the ViewModel

--- app/src/main/java/com/plcoding/mvvmtodoapp/ui/todo_list/TodoListScreen.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/ui/todo_list/TodoListScreen.kt
@@ -1,11 +1,21 @@
 package com.plcoding.mvvmtodoapp.ui.todo_list
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.plcoding.mvvmtodoapp.util.UIEvent
 
 @Composable
 fun TodoListScreen(onNavigate:(UIEvent.Navigate)-> Unit,
 viewModel: TodoListViewModel = hiltViewModel()) {
+
+    /*viewModel.todos returns a flow but we need this as compose state
+    * we use flow.collectAsState()*/
+    val todos = viewModel.todos
+
+    LaunchedEffect(key1 = true){
+
+        
+    }
 
 }
