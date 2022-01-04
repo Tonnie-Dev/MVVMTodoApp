09b062ee3016df0cf0635f2bb57f436e6bfec69c
Antony <vontonnie@gmail.com>
2022-01-04 07:24:02 +0300

    Change state to use by-delegate

--- app/src/main/java/com/plcoding/mvvmtodoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
@@ -1,28 +1,34 @@
 package com.plcoding.mvvmtodoapp.ui.add_edit_todo
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.plcoding.mvvmtodoapp.data.Todo
 import com.plcoding.mvvmtodoapp.data.TodoRepository
 import com.plcoding.mvvmtodoapp.util.UIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AddEditTodoViewModel @Inject constructor(private val repository: TodoRepository,
-                                               val savedStateHandle: SavedStateHandle) :
+class AddEditTodoViewModel @Inject constructor(
+    private val repository: TodoRepository,
+    val savedStateHandle: SavedStateHandle
+) :
     ViewModel() {
     // tracks state of clicked Todoclass
-    var todo = mutableStateOf<Todo?>(null)
-    private set
+    var todo by mutableStateOf<Todo?>(null)
+        private set
 
     //tracks state of new title and desc
-    var title = mutableStateOf("")
+    var title by mutableStateOf("")
         private set
-    var description = mutableStateOf("")
+    var description by mutableStateOf("")
         private set
 
     private val _uiEvent = Channel<UIEvent>()
@@ -35,6 +41,23 @@ class AddEditTodoViewModel @Inject constructor(private val repository: TodoRepos
         //if we open a new todoObject then we need to load it from db
 
         //retrieve id from savedStateHandle
-       val id = savedStateHandle.get<Int>("todoId")
+      savedStateHandle.get<Int>("todoId")?.let {
+
+            id ->
+
+          //check if the id is equal to -1 (default id)
+          if (id!=-1  ){
+
+              //retrieve the do from db
+
+              viewModelScope.launch {
+
+                  todo = repository.getTodoById(id)
+              }
+          }
+
+        }
+
+
     }
 }
