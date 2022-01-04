b5e02eb4d802472f15ae2db190e951ad313e27aa
Antony <vontonnie@gmail.com>
2022-01-04 07:18:09 +0300

    Retrieve todo id in init block

--- app/src/main/java/com/plcoding/mvvmtodoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
@@ -1,6 +1,7 @@
 package com.plcoding.mvvmtodoapp.ui.add_edit_todo
 
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.plcoding.mvvmtodoapp.data.Todo
 import com.plcoding.mvvmtodoapp.data.TodoRepository
@@ -11,18 +12,29 @@ import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class AddEditTodoViewModel @Inject constructor(private val repository: TodoRepository) :
+class AddEditTodoViewModel @Inject constructor(private val repository: TodoRepository,
+                                               val savedStateHandle: SavedStateHandle) :
     ViewModel() {
-
+    // tracks state of clicked Todoclass
     var todo = mutableStateOf<Todo?>(null)
     private set
 
+    //tracks state of new title and desc
     var title = mutableStateOf("")
         private set
-
     var description = mutableStateOf("")
         private set
 
     private val _uiEvent = Channel<UIEvent>()
     val uiEvent = _uiEvent.receiveAsFlow()
+
+
+    init {
+        //check if we clicked on an existing todoObject or composing a new one
+
+        //if we open a new todoObject then we need to load it from db
+
+        //retrieve id from savedStateHandle
+       val id = savedStateHandle.get<Int>("todoId")
+    }
 }
