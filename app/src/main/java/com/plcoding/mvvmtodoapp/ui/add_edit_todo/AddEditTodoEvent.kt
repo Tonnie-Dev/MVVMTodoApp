2b6f2f4b1586352418b2e7713f09adcc67713eae
Antony <vontonnie@gmail.com>
2022-01-04 08:06:05 +0300

    Add sub classes for AddEditTodoEvent sealed class

--- app/src/main/java/com/plcoding/mvvmtodoapp/ui/add_edit_todo/AddEditTodoEvent.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/ui/add_edit_todo/AddEditTodoEvent.kt
@@ -1,3 +1,8 @@
 package com.plcoding.mvvmtodoapp.ui.add_edit_todo
 
-sealed class AddEditTodoEvent
+sealed class AddEditTodoEvent {
+
+    data class OnTitleChange(val title:String):AddEditTodoEvent()
+    data class OnDescriptionChange(val description:String):AddEditTodoEvent()
+    object OnSaveTodoClick:AddEditTodoEvent()
+}
