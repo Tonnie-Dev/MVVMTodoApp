f996d269bb7f28bf4812de7dffc5af06a900b246
Antony <vontonnie@gmail.com>
2022-01-04 20:26:24 +0300

    Add Nav Host in the MainActivity

--- app/src/main/java/com/plcoding/mvvmtodoapp/MainActivity.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.plcoding.mvvmtodoapp.ui.Screens
+import com.plcoding.mvvmtodoapp.ui.add_edit_todo.AddEditTodoScreen
 import com.plcoding.mvvmtodoapp.ui.theme.MVVMTodoAppTheme
+import com.plcoding.mvvmtodoapp.ui.todo_list.TodoListScreen
+import com.plcoding.mvvmtodoapp.util.UIEvent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,8 +21,24 @@ class MainActivity : ComponentActivity() {
             MVVMTodoAppTheme {
                 
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = ) {
+                NavHost(navController = navController, startDestination = Screens.TodoListScreen.route) {
+                    
+                    
+                    composable(route = Screens.TodoListScreen.route){
+                        TodoListScreen(
+                            
+                            onNavigate =  { 
+                                
+                                UIEvent.Navigate(it.route + "/{todoId}")
+                                
+                            
+                            })
+                    }
 
+                    composable(route = Screens.AddEditTodoScreen.route + "/{todoId}"){
+                        
+                        AddEditTodoScreen(onPopBackStack = {  })
+                    }
 
                 }
 
