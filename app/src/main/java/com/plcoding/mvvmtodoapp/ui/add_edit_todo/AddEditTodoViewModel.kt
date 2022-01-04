94703b6b3eaeea365af242e73ebbfc76f1d3a93a
Antony <vontonnie@gmail.com>
2022-01-04 06:58:13 +0300

    Declare AddEditTodoViewModel states

--- app/src/main/java/com/plcoding/mvvmtodoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
@@ -4,19 +4,25 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.plcoding.mvvmtodoapp.data.Todo
 import com.plcoding.mvvmtodoapp.data.TodoRepository
+import com.plcoding.mvvmtodoapp.util.UIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class AddEditTodoViewModel @Inject constructor(private val repository: TodoRepository) :
     ViewModel() {
 
-    var todo: Todo? = mutableStateOf(null)
-        private set
+    var todo = mutableStateOf<Todo?>(null)
+    private set
 
     var title = mutableStateOf("")
         private set
 
     var description = mutableStateOf("")
         private set
+
+    private val _uiEvent = Channel<UIEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
 }
