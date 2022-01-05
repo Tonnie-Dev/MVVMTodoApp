cbb90813624c55840451456f7b056abe5c580572
Antony <vontonnie@gmail.com>
2022-01-05 07:16:05 +0300

    Override Application's Oncreate

--- app/src/main/java/com/plcoding/mvvmtodoapp/TodoApp.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/TodoApp.kt
@@ -2,6 +2,12 @@ package com.plcoding.mvvmtodoapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class TodoApp: Application()
+class TodoApp: Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant( Timber.DebugTree())
+    }
+}
